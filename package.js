041d375e5c6655da897e829ea044bd375ff82cab
ronaldbaldos989-dev <ronaldbaldos989@gmail.com>
2025-11-24 09:53:48 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "emailjs-backend",
+  "version": "1.0.0",
+  "description": "Backend server for sending EmailJS requests securely",
+  "main": "server.js",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "express": "^4.19.2",
+    "node-fetch": "^3.3.2"
+  }
+}
