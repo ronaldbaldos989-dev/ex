24e7152433ecae8cd27a8a7a51cf4587cfd90c98
ronaldbaldos989-dev <ronaldbaldos989@gmail.com>
2025-11-24 11:57:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,13 +20,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// EMAILJS ENV VALUES
+// ENV
 const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
 const EMAILJS_SERVICE = process.env.EMAILJS_SERVICE;
 const EMAILJS_TEMPLATE = process.env.EMAILJS_TEMPLATE;
 
 app.post("/send-email", async (req, res) => {
   const { template_params } = req.body;
+
+  console.log("📩 Data:", template_params);
+  console.log("🧪 Template:", EMAILJS_TEMPLATE);
 
   try {
     const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
@@ -40,9 +43,11 @@ app.post("/send-email", async (req, res) => {
       })
     });
 
+    const data = await response.text();
+    console.log("📨 EmailJS Response:", data);
+
     if (!response.ok) {
-      const errorData = await response.text();
-      return res.status(500).json({ error: "EmailJS failed", details: errorData });
+      return res.status(500).json({ error: "EmailJS failed", details: data });
     }
 
     res.json({ success: true });
@@ -52,7 +57,6 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-// Render port
 app.listen(process.env.PORT || 10000, () => {
   console.log("🚀 Server running...");
 });
